ad806cab8f10c413261579f75472d6a3e0e462f2
Tran Dang Tuan <tuan.tran@bnksolution.com>
2023-09-17 17:29:26 +0700

    Update test next to invoice

--- h26_recurring_invoice_enh/tests/test_sale_order.py
+++ h26_recurring_invoice_enh/tests/test_sale_order.py
@@ -16,41 +16,41 @@ class TestSaleOrder(TestSaleOrderCommon):
     def setUp(self):
         super(TestSaleOrder, self).setUp()
 
-    # def test_action_create_invoice(self):
-    #     for i in range(lop_number):
-    #         self.Subscriptions.action_invoice_subscription()
-    #     account_move = self.env[model].search([('invoice_line_ids.sale_line_ids.order_id', '=', self.Subscriptions.id)])
-    #     self.assertEqual(lop_number, len(account_move))
-    #
-    # def test_cron_job_recurring_invoices(self):
-    #     account_move = self.env[model].search(
-    #                 [('invoice_line_ids.sale_line_ids.order_id', '=', self.Subscriptions.id)])
-    #     for i in range(lop_number):
-    #         self.Subscriptions.next_invoice_date = date.today()
-    #         self.env['sale.order']._cron_recurring_create_invoice()
-    #     account_move1 = self.env[model].search(
-    #                 [('invoice_line_ids.sale_line_ids.order_id', '=', self.Subscriptions.id)])
-    #     self.assertEqual(lop_number + len(account_move), len(account_move1))
-    #
-    # def test_subscriptions_have_payment_exception(self):
-    #     subscriptions = self.Subscriptions.copy()
-    #     subscriptions.action_confirm()
-    #     subscriptions.payment_exception = True
-    #     account_move1 = len(self.env[model].search(
-    #         [('invoice_line_ids.sale_line_ids.order_id', '=', subscriptions.id)]))
-    #     subscriptions.action_invoice_subscription()
-    #     account_move = len(self.env[model].search([('invoice_line_ids.sale_line_ids.order_id', '=', subscriptions.id)]))
-    #     self.assertEqual(account_move1, account_move)
-    #
-    # def test_subscriptions_have_is_subscription(self):
-    #     subscriptions = self.Subscriptions.copy()
-    #     subscriptions.action_confirm()
-    #     subscriptions.is_subscription = False
-    #     account_move1 = len(self.env[model].search(
-    #         [('invoice_line_ids.sale_line_ids.order_id', '=', subscriptions.id)]))
-    #     subscriptions.action_invoice_subscription()
-    #     account_move = len(self.env[model].search([('invoice_line_ids.sale_line_ids.order_id', '=', subscriptions.id)]))
-    #     self.assertEqual(account_move1, account_move)
+    def test_action_create_invoice(self):
+        for i in range(lop_number):
+            self.Subscriptions.action_invoice_subscription()
+        account_move = self.env[model].search([('invoice_line_ids.sale_line_ids.order_id', '=', self.Subscriptions.id)])
+        self.assertEqual(lop_number, len(account_move))
+
+    def test_cron_job_recurring_invoices(self):
+        account_move = self.env[model].search(
+                    [('invoice_line_ids.sale_line_ids.order_id', '=', self.Subscriptions.id)])
+        for i in range(lop_number):
+            self.Subscriptions.next_invoice_date = date.today()
+            self.env['sale.order']._cron_recurring_create_invoice()
+        account_move1 = self.env[model].search(
+                    [('invoice_line_ids.sale_line_ids.order_id', '=', self.Subscriptions.id)])
+        self.assertEqual(lop_number + len(account_move), len(account_move1))
+
+    def test_subscriptions_have_payment_exception(self):
+        subscriptions = self.Subscriptions.copy()
+        subscriptions.action_confirm()
+        subscriptions.payment_exception = True
+        account_move1 = len(self.env[model].search(
+            [('invoice_line_ids.sale_line_ids.order_id', '=', subscriptions.id)]))
+        subscriptions.action_invoice_subscription()
+        account_move = len(self.env[model].search([('invoice_line_ids.sale_line_ids.order_id', '=', subscriptions.id)]))
+        self.assertEqual(account_move1, account_move)
+
+    def test_subscriptions_have_is_subscription(self):
+        subscriptions = self.Subscriptions.copy()
+        subscriptions.action_confirm()
+        subscriptions.is_subscription = False
+        account_move1 = len(self.env[model].search(
+            [('invoice_line_ids.sale_line_ids.order_id', '=', subscriptions.id)]))
+        subscriptions.action_invoice_subscription()
+        account_move = len(self.env[model].search([('invoice_line_ids.sale_line_ids.order_id', '=', subscriptions.id)]))
+        self.assertEqual(account_move1, account_move)
 
     def test_sub_next_to_invoice(self):
         next_invoice_date = self.Subscriptions.next_invoice_date
